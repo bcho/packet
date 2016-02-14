0a1a4ec94a75b8418e03f19e75c85066558f7b45
hbc <bcxxxxxx@gmail.com>
2016-02-14 20:55:49 +0800

    fix: add testcases.

--- packet_test.go
+++ packet_test.go
@@ -1,3 +1,63 @@
 package packet
 
-// TODO
+import (
+	"bytes"
+	"math"
+	"testing"
+)
+
+func testParser(t *testing.T, parser PacketParser, maxPacket uint32) {
+	var (
+		buf bytes.Buffer
+		err error
+	)
+
+	// simple write & read
+	data := []byte("hello")
+	err = parser.Write(&buf, data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	readData, err := parser.Read(&buf)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(readData) != string(data) {
+		t.Errorf("unexpected data read: %s", string(readData))
+	}
+
+	// write large packet data
+	largeData := make([]byte, uint64(maxPacket)+1)
+	err = parser.Write(&buf, largeData)
+	if err != ErrPacketTooLarge {
+		t.Errorf("expected error `ErrPacketTooLarge`: %v", err)
+	}
+}
+
+func TestOneByte(t *testing.T) {
+	testParser(t, OneByteLittleEndian, math.MaxUint8)
+	testParser(t, OneByteBigEndian, math.MaxUint8)
+}
+
+func TestTwoBytes(t *testing.T) {
+	testParser(t, TwoBytesLittleEndian, math.MaxUint16)
+	testParser(t, TwoBytesBigEndian, math.MaxUint16)
+}
+
+func TestFourBytes(t *testing.T) {
+	testParser(t, FourBytesLittleEndian, math.MaxUint32)
+	testParser(t, FourBytesBigEndian, math.MaxUint32)
+}
+
+func TestReadLargePacket(t *testing.T) {
+	parser := NewParser(OneByte, BigEndian)
+	parser.packetMaxLen = 10 // make error happen
+
+	largeData := make([]byte, 1)
+	largeData[0] = byte(100)
+	_, err := parser.Read(bytes.NewBuffer(largeData))
+	if err != ErrPacketTooLarge {
+		t.Errorf("expected error `ErrPacketTooLarge`: %v", err)
+	}
+}
